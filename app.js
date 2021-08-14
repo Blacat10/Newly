69f2c57a5d83acb0d5136783669bbce7ad9ad660
Blacat10 <nhilhp.ndvn@gmail.com>
2021-08-14 16:46:42 +0700

    demo new

---
+++ app.js
@@ -0,0 +1,31 @@
+let fullName = "Tran Phuong Trinh";
+let age ="16";
+let phoneNumber = "8920943";
+let favoriteThing = "music";
+
+alert(fullName);
+console.log(fullName);
+console.log(age);
+console.log(phoneNumber);
+console.log(favoriteThing);
+
+//Bài tập kiểm tra bên dưới đây
+
+let firstName = "Tran";
+let surName = "Phuong";
+let lastName ="Trinh";
+
+console.log(firstName);
+console.log(surName);
+console.log(lastName);
+
+alert(firstName);
+alert(surName);
+alert(lastName);
+
+let a = 36;
+let b = 8;
+console.log(a%b);
+
+let str = "if i were to describe a place of interest, i suppose i'd say vietnam";
+console.log(str + ' is ' + str.length);
